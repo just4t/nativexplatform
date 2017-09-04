a5a73ac7ec2fd30cd4ad0bf1403d565cf6630305
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-09-04 13:44:33 +0300

    macOS: Warn if Mono is not installed

--- build.sh
+++ build.sh
@@ -9,20 +9,23 @@ rm -rf release
 mkdir release
 
 # Get the overall version from the Unarchiver project's assembly information
-AKPKGVERSION=`cat Unarchiver/Properties/AssemblyInfo.cs | grep "^\[assembly: AssemblyVersion(\"[^\"]*" --color=never -o | grep -o --color=never -e "[0-9\.]*" | sed 's/\.$//'`
+AKPKGVERSION=`cat Unarchiver/Properties/AssemblyInfo.cs | grep "^\[assembly: AssemblyVersion(\"[^\"]*" --color=never -o | grep -o --color=never "\d.\d.\d"`
 
 # Akeeba eXtract Wizard for Linux
 zip -j release/extract-wizard-linux-$AKPKGVERSION.zip ExtractWizardGtk/bin/Release/*.exe ExtractWizardGtk/bin/Release/*.dll
 
 # Akeeba eXtract Wizard for macOS
 pushd release
+mkdir 000
 cp -Rf ../ExtractWizardGtk/macOS/Akeeba\ eXtract\ Wizard.app .
 cp ../ExtractWizardGtk/bin/Release/*.exe Akeeba\ eXtract\ Wizard.app/Contents/Resources/
 cp ../ExtractWizardGtk/bin/Release/*.dll Akeeba\ eXtract\ Wizard.app/Contents/Resources/
-cd Akeeba\ eXtract\ Wizard.app
+mv Akeeba\ eXtract\ Wizard.app 000
+cd 000
 zip -r ../extract-wizard-macos-$AKPKGVERSION.zip *
+cd ..
+rm -rf 000
 popd
-rm -rf release/Akeeba\ eXtract\ Wizard.app
 
 # Akeeba eXtract CLI for macOS and Linux
 zip -j release/extract-cli-macos-linux-$AKPKGVERSION.zip extractCLI/bin/Release/*.exe extractCLI/bin/Release/*.dll
