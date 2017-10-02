df57c56cd689251ed5578cc5877af19d393bf09d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-10-02 23:55:57 +0300

    Version bump

--- Unarchiver/Properties/AssemblyInfo.cs
+++ Unarchiver/Properties/AssemblyInfo.cs
@@ -55,7 +55,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.0.3.*")]
-[assembly: AssemblyFileVersion("4.0.3.*")]
+[assembly: AssemblyVersion("4.0.4.*")]
+[assembly: AssemblyFileVersion("4.0.4.*")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
--- extractCLI/Properties/AssemblyInfo.cs
+++ extractCLI/Properties/AssemblyInfo.cs
@@ -67,6 +67,6 @@ using CommandLine;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.0.3.*")]
-[assembly: AssemblyFileVersion("4.0.3.*")]
+[assembly: AssemblyVersion("4.0.4.*")]
+[assembly: AssemblyFileVersion("4.0.4.*")]
 [assembly: NeutralResourcesLanguage("en-US")]
